191263e3e2f0763b0175f3c7772cf5c4fddc14e5
Jonny Hall <car87maniac@googlemail.com>
2022-11-07 01:37:40 +0000

    Feedback Stats done

--- src/components/Shared/FeedbackStats.jsx
+++ src/components/Shared/FeedbackStats.jsx
@@ -1,10 +1,23 @@
-function FeedbackStats(feedback) {
+import PropTypes from "prop-types";
+function FeedbackStats({ feedback }) {
+  //calculating average review
+  let averageRating =
+    feedback.reduce((accumulator, currentValue) => {
+      return accumulator + currentValue.rating;
+    }, 0) / feedback.length;
+  // 0 is the defaut for the accumulator
+  console.log(averageRating);
+  averageRating = averageRating.toFixed(1).replace(/[.,]0$/, "");
+  // THIS will only show a decimail if it is not zero
   return (
     <div className="feedback-stats">
-      <h4> Reviews {feedback.length}</h4>
-      <h4>Average Rating</h4>
+      <h4> Reviews: {feedback.length}</h4>
+      <h4>Rating: {isNaN(averageRating) ? 0 : averageRating}</h4>
     </div>
   );
 }
 
+FeedbackStats.propTypes = {
+  feedback: PropTypes.array.isRequired,
+};
 export default FeedbackStats;
