ef412c3c72463e75c3d7b6f2f0f6166977002132
Jonny Hall <car87maniac@googlemail.com>
2023-01-04 21:34:22 +0000

    db.json not working with backend somehow

--- src/index.js
+++ src/index.js
@@ -6,3 +6,28 @@ import App from "./App";
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
+
+{
+  /*
+{
+  "feedback": [
+    {
+      "id": 1,
+      "rating": 10,
+      "text": "Backend Feedback 1"
+    },
+    {
+      "id": 2,
+      "rating": 2,
+      "text": "Backend Feedback 2"
+    },
+    {
+      "id": 3,
+      "rating": 4,
+      "text": "Backend Feedback 3"
+    }
+  ]
+}
+
+*/
+}
