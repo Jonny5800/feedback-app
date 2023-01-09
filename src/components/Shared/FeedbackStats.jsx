4e5cab5da5486d5dc500323e27fa06f9f60ded6d
Jonny Hall <car87maniac@googlemail.com>
2023-01-09 01:51:09 +0000

    fixed avg rating

--- src/components/Shared/FeedbackStats.jsx
+++ src/components/Shared/FeedbackStats.jsx
@@ -12,13 +12,17 @@ function FeedbackStats() {
       return accumulator + currentValue.rating;
     }, 0) / feedback.length;
   // 0 is the defaut for the accumulator
-  console.log(averageRating);
+  console.log(averageRating + "this is the average rating");
   //averageRating = averageRating.toFixed(1).replace(/[.,]0$/, "");
   // THIS will only show a decimail if it is not zero
+
   return (
     <div className="feedback-stats">
       <h4> Reviews: {feedback.length}</h4>
-      <h4>Rating: {isNaN(averageRating) ? 0 : averageRating}</h4>
+      <h4>
+        Rating:{" "}
+        {isNaN(averageRating) ? 0 : Math.round(averageRating * 100) / 100}
+      </h4>
     </div>
   );
 }
